ee3d727301f7f4c98fd1eba8bbed3c18bbaa1075
luceraCalcio <luceracalcio2023@gmail.com>
2023-08-27 05:03:25 -0700

    Update App.jsx

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,18 +8,49 @@ const Head = (props) => {
 
    );
 }
- 
+
 
 // Content Component
-const Content = (props) => {
-  console.log(props)
+const Content = () => {
+
   return (
     <div>
-      <p> {props.part} {props.exercises}</p>
+      <Part1 />
+      <Part2  />
+      <Part3 />
     </div>
    );
 }
  
+
+//Refactoring Contet component
+const Part1 = () => {
+  return ( 
+    <div>
+      <p> {part1} {exercises1}</p>
+    </div>
+   );
+}
+
+const Part2 = () => {
+  return ( 
+    <div>
+      <p> {part2} {exercises2}</p>
+    </div>
+   );
+}
+
+const Part3 = () => {
+  return ( 
+    <div>
+      <p> {part3} {exercise3}</p>
+    </div>
+   );
+}
+
+
+
+
 
 // // Total Component
 const Total = (props) => {
@@ -49,9 +80,10 @@ const App = () => {
   return (
     <div>
      <Head course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+     <Content />
+      {/* <Content part={part1} exercises={exercises1} /> */}
+      {/* <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} /> */}
 
       <Total exercises = {exercises1 + exercises2 + exercises3}/>
       
